06b005893a53d40a1544613b94616f7580801f07
samd <sershdiaz@hotmail.com>
2025-02-05 14:20:01 -0600

    archivo funciones.php creado e index.php agregado

---
+++ practicas/p06/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+
+        require_once __DIR__ .'/src/funciones.php';
+
+        if(isset($_GET['numero']))
+        {
+            es_multiplo7y5($_GET['numero']);
+            
+        }
+    ?>
+
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        if(isset($_POST["name"]) && isset($_POST["email"]))
+        {
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
+        }
+    ?>
+</body>
+</html>
