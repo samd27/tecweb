c2869d011ef6d2c6bb69b2df1a3f0f8a6889685c
samd <sershdiaz@hotmail.com>
2025-02-19 14:47:40 -0600

    ejercicio 2 hecho de forma parcial

---
+++ practicas/p08/set_producto_v2.php
@@ -0,0 +1,38 @@
+<?php
+$nombre   = $_POST['nombre'];
+$marca    = $_POST['marca'];
+$modelo   = $_POST['modelo'];
+$precio   = $_POST['precio'];
+$detalles = $_POST['detalles'];
+$unidades = $_POST['unidades'];
+$imagen   = $_POST['imagen'];
+
+/** SE CREA EL OBJETO DE CONEXION */
+@$link = new mysqli('localhost', 'root', 'samd2704', 'marketzone');	
+
+/** comprobar la conexión */
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+}
+
+$result = $link->query("SELECT * FROM productos WHERE nombre = '{$nombre}'");
+if ($result->num_rows > 0) 
+{
+    die('El producto ya existe');
+}
+
+/** Crear una tabla que no devuelve un conjunto de resultados */
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+if ( $link->query($sql) ) 
+{
+    echo 'Producto insertado con ID: '.$link->insert_id;
+}
+else
+{
+	echo 'El Producto no pudo ser insertado =(';
+}
+
+$link->close();
+?>
