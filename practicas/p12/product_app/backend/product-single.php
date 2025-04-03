41b1261e97af3c97dcfa4277fae354ce1127b13c
samd <sershdiaz@hotmail.com>
2025-04-03 14:47:56 -0600

    Practica 12 - Archivos innecesarios eliminados

--- practicas/p12/product_app/backend/product-single.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    use TECWEB\MYAPI\Products as Products;
-    require_once __DIR__ . '/myapi/Products.php';
-
-    $prodObj = new Products('root','samd2704','marketzone');
-    $prodObj->single($_POST['id']);
-    echo $prodObj->getData();
-?>
